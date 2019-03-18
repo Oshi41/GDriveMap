2e200c1255c18c07f33ba5c7b9b622ee76164ea1
oshi4 <sheglovaoe@gmail.com>
2019-03-18 23:31:27 +0300

    Add project files.

---
+++ GDriveFiles/Classes/FileNode.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Sheets.v4.Data;
+
+namespace GDriveFiles.Classes
+{
+    class FileNode
+    {
+        private readonly DriveService _service;
+
+        private readonly string _folderType = "application/vnd.google-apps.folder";
+
+        public FileNode(DriveService service, File file)
+        {
+            _service = service;
+            File = file;
+        }
+
+        public async Task Search()
+        {
+            if (!_folderType.Equals(File.MimeType))
+                return;
+
+            string token = null;
+
+            do
+            {
+
+                var request = _service.Files.List();
+                //API max
+                request.PageSize = 1000;
+                request.Q = $"'{File.Id}' in parents";
+                request.Fields = $"nextPageToken, files({FileFields})";
+                request.PageToken = token;
+
+                var result = await request.ExecuteAsync();
+
+                if (result.Files.Any())
+                {
+                    Children.AddRange(result.Files.Select(x => new FileNode(_service, x)));
+                }
+
+                token = result.NextPageToken;
+
+            } while (token != null);
+
+            Console.WriteLine($"{File.Name} - {Children.Count} files/folders");
+
+            var tasks = Children.Select(x => x.Search());
+            await Task.WhenAll(tasks);
+
+            
+        }
+
+        public List<List<string>> GetRows(uint depth = 0)
+        {
+            var result = new List<List<string>>();
+
+            // В каждой строке 2 столбца - Имя и ссылка на просмотр
+            var row = new List<string>
+            {
+                File.Name,
+                File.WebViewLink
+            };
+
+            // каждая следующая ячейка должна быть с отступом
+            for (int i = 0; i < depth; i++)
+            {
+                row.Insert(0, string.Empty);
+            }
+
+            result.Add(row);
+
+            // Сначала показываю папки
+            foreach (var child in Children.Where(x => x.Children.Any()))
+            {
+                result.AddRange(child.GetRows(depth + 1));
+            }
+
+            // Потом идут файлы
+            foreach (var child in Children.Where(x => !x.Children.Any()))
+            {
+                result.AddRange(child.GetRows(depth + 1));
+            }
+
+            return result;
+        }
+
+        public File File { get; }
+        public List<FileNode> Children { get; } = new List<FileNode>();
+
+        public static string FileFields { get; } = "id, name, webViewLink, parents, mimeType";
+    }
+}
---
+++ GDriveFiles/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using GDriveFiles.Classes;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Json;
+using Google.Apis.Logging;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+
+namespace GDriveFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = ParseFolder(args);
+
+            Authorize();
+
+            var request = _driveService.Files.List();
+            request.PageSize = 10;
+            request.Q = $"name='{name}'";
+            request.Fields = $"files({FileNode.FileFields})";
+
+            var response = request.Execute();
+            var root = response.Files.First();
+
+            var node = new FileNode(_driveService, root);
+
+            Task.WaitAll(node.Search());
+
+            // Получили всю карту папки 
+            var rows = node.GetRows();
+
+            CreateAndWriteSheet(rows);
+        }
+
+        static void CreateAndWriteSheet(List<List<string>> rows)
+        {
+            var name = $"Google Drive Map on {DateTime.Now:dd.MM.yyyy hh:mm:ss}";
+
+            var spreadSheet = new Spreadsheet
+            {
+                Properties = new SpreadsheetProperties
+                {
+                    Title = name,
+                },
+
+                Sheets = new List<Sheet>
+                {
+                    new Sheet
+                    {
+                        Properties = new SheetProperties
+                        {
+                            Title = "Main",
+                        },
+                    }
+                },
+            };
+
+            var sheetRequest = _sheetService.Spreadsheets.Create(spreadSheet);
+            sheetRequest.Fields = _sheetsFields;
+            spreadSheet = sheetRequest.Execute();
+
+            var values = new ValueRange
+            {
+                Values = rows.Select(x => (IList<object>)x.Cast<object>().ToList()).ToList(),
+                MajorDimension = "ROWS",
+                Range = "Main",
+            };
+
+            var updReq = _sheetService.Spreadsheets.Values.Update(values, spreadSheet.SpreadsheetId,
+                values.Range);
+            updReq.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+            updReq.Fields = _sheetsFields;
+            var updResponse = updReq.Execute();
+
+            var createdSpredsheetRequest = _sheetService.Spreadsheets.Get(spreadSheet.SpreadsheetId);
+            createdSpredsheetRequest.Fields = _sheetsFields;
+            var createdSpredsheet = createdSpredsheetRequest.Execute();
+
+            Console.WriteLine($"Created spreadsheet - {name}");
+        }
+
+        static string ParseFolder(string[] args)
+        {
+            // Сначала проверяем агрументы
+            if (args.Any() && !string.IsNullOrEmpty(args[0]))
+            {
+                return args[0];
+            }
+
+            Console.WriteLine("Введите точное название папки для составления её карты");
+            var name = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name))
+                throw new Exception("Введите правильное имя папки");
+
+            return name;
+        }
+
+        private static DriveService _driveService;
+        private static SheetsService _sheetService;
+        private static string _sheetsFields = "spreadsheetId";
+
+        private static void Authorize()
+        {
+            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(new MemoryStream(Properties.Resources.workingCredentials)).Secrets,
+                    new[] 
+                    {
+                        DriveService.Scope.DriveMetadataReadonly,
+                        SheetsService.Scope.Spreadsheets
+                    },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore("tokens", true))
+                .Result;
+
+            var initializer = new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = Assembly.GetExecutingAssembly().GetName().Name
+            };
+
+            _driveService = new DriveService(initializer);
+            _sheetService = new SheetsService(initializer);
+        }
+    }
+}
